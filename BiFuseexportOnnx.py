f41548587ca74d0335e4c3844704820ccb97b9ab
yusuku <121999446+yusuku@users.noreply.github.com>
2024-11-25 00:15:58 +0900

    Error
      File "C:\Users\yusuke\anaconda3\envs\fre\Lib\site-packages\torch\onnx\utils.py", line 1848, in _run_symbolic_function
        raise errors.UnsupportedOperatorError(
    torch.onnx.errors.UnsupportedOperatorError: Exporting the operator 'aten::grid_sampler' to ONNX opset version 15 is not supported. Support for this operator was added in version 16, try exporting with this version.

--- BiFuseexportOnnx.py
+++ BiFuseexportOnnx.py
@@ -50,6 +50,19 @@ def Run(loader, model):
             
             if it==0:
                 break
+    
+    print("export start")
+    torch.onnx.export(model,               # model being run
+                      inputs,                         # model input (or a tuple for multiple inputs)
+                      "super_resolution.onnx",   # where to save the model (can be a file or file-like object)
+                      export_params=True,        # store the trained parameter weights inside the model file
+                      opset_version=15,          # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names = ['input'],   # the model's input names
+                      output_names = ['output'], # the model's output names
+                      dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
+                                    'output' : {0 : 'batch_size'}})
+    print("export end")
             
 
 def main():
