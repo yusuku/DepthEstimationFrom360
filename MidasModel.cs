9825f1e1f00cbfaafa235226a05efd0fb692a251
yusuku <121999446+yusuku@users.noreply.github.com>
2024-12-20 22:25:45 +0900

    Correct memory leak

--- MidasModel.cs
+++ MidasModel.cs
@@ -7,47 +7,51 @@ using UnityEngine;
 using UnityEngine.Assertions;
 public class MidasModel : MonoBehaviour
 {
-    // Model 
+    // モデルとテクスチャの関連リソース
     public ModelAsset modelAsset;
-    public RenderTexture inputtex;
+    public Texture2D inputtex;
     public RenderTexture outputTexture;
-    Worker m_Worker;
-    Tensor<float> inputTensor;
-
     public Material mat;
 
+    private Worker m_Worker;
+    private Model model;
 
-   
+
 
     void Start()
     {
-        
-        
+        model = ModelLoader.Load(modelAsset);
+        m_Worker = new Worker(model, BackendType.GPUCompute);
     }
 
     void Update()
     {
-        model2Text(modelAsset, inputtex);
+        ProcessModel(inputtex);
     }
 
     void OnDisable()
     {
-     
+        m_Worker.Dispose();
     }
 
-   
-    void model2Text(ModelAsset modelAsset, RenderTexture inputtex)
-    {
-        inputTensor = TextureConverter.ToTensor(inputtex, width: 256, height: 256,channels: 3);
 
-        Model model = ModelLoader.Load(modelAsset);
-        m_Worker = new Worker(model, BackendType.GPUCompute);
-        m_Worker.Schedule(inputTensor);
-        Tensor<float> outputTensor = m_Worker.PeekOutput() as Tensor<float>;
-        outputTensor.Reshape(new TensorShape(1,outputTensor.shape[0],outputTensor.shape[1], outputTensor.shape[2]));
-        Debug.Log(outputTensor.shape);
-        mat.mainTexture= TextureConverter.ToTexture(outputTensor);
-        
+    void ProcessModel(Texture2D inputtex)
+    {
+        using (Tensor<float> inputTensor = TextureConverter.ToTensor(inputtex, width: 256, height: 256, channels: 3))
+        {
+            
+            
+            m_Worker.Schedule(inputTensor);
+            using (Tensor<float> outputTensor = m_Worker.PeekOutput() as Tensor<float>)
+            {
+                if (outputTensor != null)
+                {
+                    outputTensor.Reshape(new TensorShape(1, outputTensor.shape[0], outputTensor.shape[1], outputTensor.shape[2]));
+                    Debug.Log(outputTensor.shape);
+                    mat.mainTexture = TextureConverter.ToTexture(outputTensor);
+                }
+            }
+        }        
     }
     
 }
